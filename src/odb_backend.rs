95ca1a6d476bed5e108c4a51def4480142149bec
Konni <hi@kartonrad.de>
2026-04-07 10:58:57 +0200

    More Docs from libgit2, more bindings

--- src/odb_backend.rs
+++ src/odb_backend.rs
@@ -1,9 +1,16 @@
 #![allow(dead_code)]
-use std::{any::Any, ffi::c_void, marker::PhantomData, ptr::NonNull};
+use std::{
+    any::Any,
+    ffi::c_void,
+    marker::PhantomData,
+    ptr::{slice_from_raw_parts, NonNull},
+};
 
-use libc::{c_int, size_t};
+use libc::{c_int, c_uint, size_t};
 use libgit2_sys::git_oid;
 pub use libgit2_sys::{git_object_t, git_odb_backend, git_odb_backend_data_alloc};
+
+use crate::{Binding, Error, Oid};
 
 // TODO: Lifetimes: with phantom data!!!!
 
@@ -31,6 +38,12 @@ mod ee {
     pub struct git_odb_backendd {
         pub version: c_uint,
         pub odb: *mut git_odb,
+        /// Read an object from the database.
+        ///
+        /// This method queries all available ODB backends trying to read the given OID.
+        ///
+        /// The returned object is reference counted and internally cached,
+        /// so it should be closed by the user once it's no longer in use.
         ///
         /// # returns
         /// `0` or an error code
@@ -262,20 +275,46 @@ mod ee {
         /// then the given packfile will likely be streamed directly to disk (and a corresponding index created).
         /// If the ODB layer does not understand pack files,
         /// the objects will be stored in whatever format the ODB layer uses.
+        ///
+        /// ## returns
+        /// 0 or an error code.
         pub writepack: Option<
             extern "C" fn(
+                // `out`: [OUT] pointer to the writepack functions
                 *mut *mut git_odb_writepack,
+                // `backend`: reference to the odb backend
                 *mut git_odb_backend,
+                // `db`: object database where the stream will read from
                 *mut git_odb,
+                // `progress_cb`: function to call with progress information
+                // be aware that this is called inline with network and indexing operations,
+                // so performance may be affected.
                 git_indexer_progress_cb,
+                // `progress_payload`: payload for the progress callback
                 *mut c_void,
             ) -> c_int,
         >,
 
+        /// Write a multi-pack-index file from all the .pack files in the ODB.
+        ///
+        /// If the ODB layer understands pack files,
+        /// then this will create a file called multi-pack-index next to the .pack and .idx files,
+        /// which will contain an index of all objects stored in .pack files.
+        /// This will allow for O(log n) lookup for n objects (regardless of how many packfiles there exist).
+        ///
+        /// ## returns
+        /// 0 or an error code
         pub writemidx: Option<extern "C" fn(*mut git_odb_backend) -> c_int>,
 
+        /// "Freshens" an already existing object, updating its last-used time.
+        /// This occurs when git_odb_write was called, but the object already existed (and will not be re-written).
+        /// The underlying implementation may want to update last-used timestamps.
+        ///
+        /// If callers implement this, they should return 0 if the object exists and was freshened, and non-zero otherwise.
         pub freshen: Option<extern "C" fn(*mut git_odb_backend, *const git_oid) -> c_int>,
 
+        /// Frees any resources held by the odb (including the git_odb_backend itself).
+        /// An odb backend implementation must provide this function.
         pub free: Option<extern "C" fn(*mut git_odb_backend)>,
     }
 }
@@ -336,31 +375,96 @@ pub struct OdbBackendSneakstructure {
     pub rust_impl: Box<dyn OdbBackend>,
 }
 
+/// This Trait allows the implementer to provide an ODB-Backend-Implementation which will
+/// be called by libgit2's C code.
+///
+/// The Type implementing OdbBackend can keep state,
+/// and can be registered in a [crate::Odb] using [crate::Odb::add_backend].
 pub trait OdbBackend: 'static {
-    // const VERSION: c_uint;
+    /// Get the ODB Backend Version
+    fn get_version(&self) -> c_uint;
 
-    // pub fn new() -> Self;
+    /// Read an object from the database.
+    fn read(&self, oid: &Oid) -> Result<GitRawObj, Error>;
 
-    fn read(&self, oid: &git_oid) -> GitRawObj;
+    /// Read an object from the database, given a prefix of its identifier.
+    /// This method queries the ODB backend trying to match the 'len'
+    /// first hexadecimal characters of the 'short_id'. The remaining (GIT_OID_SHA1_HEXSIZE-len)*4 bits of
+    /// 'short_id' must be 0s.
+    ///
+    /// 'len' must be at least GIT_OID_MINPREFIXLEN,
+    /// the method will fail otherwise.
+    /// and the prefix must be long enough to identify a unique object in all the backends;
+    ///
+    /// # Returns
+    /// The matched OID and Object, or Error with
+    /// [ErrorCode::NotFound] is not found, [ErrorCode::Ambiguous] if multiple matches were found
+    fn read_prefix(&self, oid: &git_oid, oid_prefix_len: size_t)
+        -> Result<(Oid, GitRawObj), Error>;
 
-    //pub fn read_prefix(oid: &git_oid) -> GitRawObj {}
+    /// Read the header of an object from the database, without reading its full contents.
+    ///
+    /// The header includes the length and the type of an object.
+    ///
+    /// Note that most backends do not support reading only the header of an object,
+    /// so the whole object will be read and then the header will be returned.
+    fn read_header(&self, oid: &Oid) -> Result<GitObjectInfo, Error>;
 
-    //pub fn find_from_short_oid(short_oid: &git_oid, len: size_t)
+    /// Write an object directly into the ODB
+    ///
+    /// This method writes a full object straight into the ODB.
+    /// For most cases, it is preferred to write objects through a write stream,
+    /// which is both faster and less memory intensive, specially for big objects.
+    ///
+    /// This method is provided for compatibility
+    /// with custom backends which are not able to support streaming writes
+    fn write(&self, oid: &Oid, buffer: &[u8], object_type: git_object_t) -> Result<(), Error>;
 
-    fn read_header(&self, oid: &git_oid) -> GitObjectInfo;
+    // TODO: writestream
+    //       readstream
 
-    fn exists(&self, oid: &git_oid) -> bool;
+    /// Determine if the given object can be found in the object database.
+    fn exists(&self, oid: &Oid) -> bool;
 
+    /// Determine if an object can be found in the object database by an abbreviated object ID.
+    ///
+    /// # Returns
+    /// The full matched oid, or Error with
+    /// [ErrorCode::NotFound] is not found, [ErrorCode::Ambiguous] if multiple matches were found
+    fn exists_prefix(&self, oid: &git_oid, len: size_t) -> Result<Option<git_oid>, Error>;
+
+    /// Refresh the object database to load newly added files.
+    ///
+    /// If the object databases have changed on disk while the library is running,
+    /// this function will force a reload of the underlying indexes.
+    ///
+    /// Use this function when you're confident that an external application has tampered with the ODB.
+    ///
+    /// NOTE that it is not necessary to call this function at all.
+    /// The library will automatically attempt to refresh the ODB when a lookup fails,
+    /// to see if the looked up object exists on disk but hasn't been loaded yet.
+    fn refresh(&self) -> Result<(), Error>;
+
+    // TODO: fn foreach
+    // TODO: fn writepack
+    // TODO: fn writemidx
+    // TODO: fn freshen
+
+    /// Cast "dyn OdbBackend" to "dyn Any"
     fn as_any_mut(&self) -> &mut dyn Any;
-
-    fn exists_prefix(&self, oid: &git_oid) -> Option<git_oid>;
-
-    fn write(&self, oid: &git_oid, buffer: &Vec<u8>, object_type: git_object_t);
-
-    fn refresh(&self);
 }
 
 // ===== Interface Methods: Static Functions that allow git2 to call back to the Rust Trait-Methods ======
+
+/// Read an object from the database.
+///
+/// This method queries all available ODB backends trying to read the given OID.
+///
+/// The returned object is reference counted and internally cached,
+/// so it should be closed by the user once it's no longer in use.
+///
+/// # returns
+/// `0` or an error code
 pub extern "C" fn _git_dyn_odbbackend_read(
     // allocate a git_rawobj here
     output_buffer: *mut *mut c_void,
@@ -381,7 +485,16 @@ pub extern "C" fn _git_dyn_odbbackend_read(
             .expect("GIT-ODB-Backend read should never be called with a null pointer");
 
         // Call Trait Method
-        let read = odb_backend_ref.rust_impl.read(&*requested_oid); // TODO: Allow various errors
+        let read = match odb_backend_ref
+            .rust_impl
+            .read(&Oid::from_raw(requested_oid))
+        {
+            Ok(read) => read,
+            Err(git_err) => {
+                let err_code = git_err.raw_set_git_error();
+                return err_code;
+            }
+        };
 
         // Convert Results
         let buffer: *mut u8 = git_odb_backend_data_alloc(backend_ref, read.data.len()) as *mut u8;
@@ -389,18 +502,188 @@ pub extern "C" fn _git_dyn_odbbackend_read(
 
         buffer.copy_from_slice(&read.data);
 
-        *output_buffer = buffer.as_mut_ptr() as *mut c_void;
-        *output_buffer_size = read.data.len(); // Special treatement for size_t ?
+        *output_buffer = buffer.as_mut_ptr().cast();
+        *output_buffer_size = read.data.len() as size_t;
         *output_type = read.git_object_type;
 
         return 0;
     }
 }
 
+/// Read an object from the database, given a prefix of its identifier.
+/// This method queries the ODB backend trying to match the 'len'
+/// first hexadecimal characters of the 'short_id'. The remaining (GIT_OID_SHA1_HEXSIZE-len)*4 bits of
+/// 'short_id' must be 0s.
+///
+/// 'len' must be at least GIT_OID_MINPREFIXLEN,
+/// and the prefix must be long enough to identify a unique object in all the backends;
+/// the method will fail otherwise.
+///
+/// The returned object is reference counted and internally cached,
+/// so it should be closed by the user once it's no longer in use.
+///
+/// # Arguments
+/// * `obj_id`: [OUT] the id of the object that was found
+/// * `obj_buffer`: [OUT] pointer where to store the read object
+/// * `obj_buffer_len`: [OUT] length of read object
+/// * `obj_type`: [OUT] type of the read object
+/// * `backend`: reference to the odb backend
+/// * `oid_prefix`: a prefix of the id of the object to read
+/// * `oid_prefix_len`: the length of the prefix
+///
+/// # returns
+/// `0` or an error code
+pub extern "C" fn _git_dyn_odbbackend_read_prefix(
+    obj_id: *mut git_oid,
+    obj_buffer: *mut *mut c_void,
+    obj_buffer_len: *mut size_t,
+    obj_type: *mut git_object_t,
+    backend: *mut git_odb_backend,
+    oid_prefix: *const git_oid,
+    oid_prefix_len: size_t,
+) -> c_int {
+    unsafe {
+        // Obtain Reference
+        let odb_backend_ref: &mut OdbBackendSneakstructure = (backend
+            as *mut OdbBackendSneakstructure)
+            .as_mut()
+            .expect("GIT-ODB-Backend read_prefix should never be called with a null pointer");
+
+        // Call Trait Method
+        let read = match odb_backend_ref
+            .rust_impl
+            .read_prefix(&*oid_prefix, oid_prefix_len)
+        {
+            Ok(read) => read,
+            Err(git_err) => {
+                let err_code = git_err.raw_set_git_error();
+                return err_code;
+            }
+        };
+
+        // Convert Results
+        let buffer: *mut u8 = git_odb_backend_data_alloc(backend, read.1.data.len()) as *mut u8;
+        let buffer = std::slice::from_raw_parts_mut(buffer, read.1.data.len());
+
+        buffer.copy_from_slice(&read.1.data);
+
+        *obj_id = *read.0.raw();
+        *obj_buffer = buffer.as_mut_ptr().cast();
+        *obj_buffer_len = read.1.data.len() as size_t;
+        *obj_type = read.1.git_object_type;
+
+        return 0;
+    }
+}
+
+/// Read the header of an object from the database, without reading its full contents.
+///
+/// The header includes the length and the type of an object.
+///
+/// Note that most backends do not support reading only the header of an object,
+/// so the whole object will be read and then the header will be returned.
+///
+/// # Arguments
+/// * `len_out`: [OUT] pointer where to store the length
+/// * `type_out`: [OUT] pointer where to store the type
+/// * `backend`: reference to the odb backend
+/// * `id`: identity of the object to read
+///
+/// # returns
+/// `0` or an error code
+pub extern "C" fn _git_dyn_odbbackend_read_header(
+    len_out: *mut size_t,
+    type_out: *mut git_object_t,
+    backend: *mut git_odb_backend,
+    obj_id: *const git_oid,
+) -> c_int {
+    unsafe {
+        // Obtain Reference
+        let odb_backend_ref: &mut OdbBackendSneakstructure = (backend
+            as *mut OdbBackendSneakstructure)
+            .as_mut()
+            .expect("GIT-ODB-Backend read_header should never be called with a null pointer");
+
+        // Call Trait Method
+        let object_info = match odb_backend_ref
+            .rust_impl
+            .read_header(&Oid::from_raw(obj_id))
+        {
+            Ok(object_info) => object_info,
+            Err(git_err) => {
+                let err_code = git_err.raw_set_git_error();
+                return err_code;
+            }
+        };
+
+        // Convert Results
+        *len_out = object_info.size;
+        *type_out = object_info.git_object_type;
+
+        return 0;
+    }
+}
+
+/// Write an object directly into the ODB
+///
+/// This method writes a full object straight into the ODB.
+/// For most cases, it is preferred to write objects through a write stream,
+/// which is both faster and less memory intensive, specially for big objects.
+///
+/// This method is provided for compatibility
+/// with custom backends which are not able to support streaming writes
+///
+/// # Arguments
+/// * `backend`: reference to the odb backend
+/// * `id`: under which id to store the data
+///   (since this is a *const, i assume the hashing was already done.)
+/// * `data`: buffer with the data to store
+/// * `len`: Size of the buffer
+/// * `type`: type of the data to store
+///
+/// # returns
+/// `0` or an error code
+pub extern "C" fn _git_dyn_odbbackend_write(
+    backend: *mut git_odb_backend,
+    id: *const git_oid,
+    data: *const c_void,
+    len: size_t,
+    r#type: git_object_t,
+) -> c_int {
+    unsafe {
+        // Obtain Reference
+        let odb_backend_ref: &mut OdbBackendSneakstructure = (backend
+            as *mut OdbBackendSneakstructure)
+            .as_mut()
+            .expect("GIT-ODB-Backend write should never be called with a null pointer");
+
+        let buffer_slice: &[u8] = slice_from_raw_parts(std::mem::transmute(data), len)
+            .as_ref()
+            .expect("Reference must not be null");
+
+        // Call Trait Method
+        match odb_backend_ref
+            .rust_impl
+            .write(&Oid::from_raw(id), buffer_slice, r#type)
+        {
+            Ok(()) => (),
+            Err(git_err) => {
+                let err_code = git_err.raw_set_git_error();
+                return err_code;
+            }
+        };
+
+        return 0;
+    }
+}
+
+/// Frees any resources held by the odb (including the git_odb_backend itself).
+/// An odb backend implementation must provide this function.
 pub extern "C" fn _git_dyn_odbbackend_free(backend_ref: *mut git_odb_backend) {
     unsafe {
         // Take ownership and drop.
-        let object_database_backend = Box::from_raw(backend_ref);
+        let object_database_backend: Box<OdbBackendSneakstructure> =
+            Box::from_raw(backend_ref.cast());
         drop(object_database_backend)
     }
 }
